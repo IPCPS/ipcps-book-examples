90544c2dab529d9b95c65588e2fe3985fa95c64c
Joe Politz <jpolitz@eng.ucsd.edu>
2021-08-13 12:45:50 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
 javac -cp /usr/local/bin/tester.jar HelloWorld.java
-echo 'TECHIO> terminal -i "java -classpath /usr/local/bin/tester.jar:. tester.Main HelloWorld"'
-sleep 10
-echo 'TECHIO> success STATUS'
+java -classpath /usr/local/bin/tester.jar:. tester.Main HelloWorld
