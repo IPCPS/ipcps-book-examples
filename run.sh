f297915cc9bc7ae03dc581260ebc0bf43deaa7db
Joe Politz <jpolitz@eng.ucsd.edu>
2021-09-03 11:55:03 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-echo "TECHIO> terminal bash -i 'javac -cp /usr/local/bin/tester.jar $1.java && java -classpath /usr/local/bin/tester.jar:. tester.Main $1'"
+javac -cp /usr/local/bin/tester.jar $1.java && java -classpath /usr/local/bin/tester.jar:. tester.Main $1
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 
 
